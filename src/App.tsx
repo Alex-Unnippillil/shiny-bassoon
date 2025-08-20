1909ec4007ed64df9ba1483f6c6151b83c97d89b
alex.unnippillil <alex.unnippillil@hotmail.com>
2025-08-20 00:59:31 -0400

    Fix conflicts: removed problematic lines from 6 files

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,1 @@
-import React from 'react';
-import ChessGame from './components/ChessGame';
-
-export default function App(): JSX.Element {
-  return <ChessGame />;
-}
+﻿
--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,1 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import './index.css';
-import { ThemeProvider, createTheme } from '@mui/material/styles';
-import { GameProvider } from './store';
-
-const theme = createTheme();
-
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-      <ThemeProvider theme={theme}>
-        <GameProvider>
-          <App />
-        </GameProvider>
-      </ThemeProvider>
-    </React.StrictMode>,
-  );
+﻿
